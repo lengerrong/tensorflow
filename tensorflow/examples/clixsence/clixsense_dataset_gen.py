c6843cd9ceb16ca8df8f7634bc00d7de11e5f8e6
errong.leng <errong.leng@gmail.com>
2017-04-12 16:57:35 +0800

    clixsense cnn traning mode
    138 train data
    128 eval data
    got just 25% accuracy
    
    need more train

--- tensorflow/examples/clixsence/clixsense_dataset_gen.py
+++ tensorflow/examples/clixsence/clixsense_dataset_gen.py
@@ -42,31 +42,59 @@ def append_clixsense_dataset(filename, dataset, iscat):
   out  = np.array(list(label) + list(r) + list(g) + list(b), np.uint8)
   out.tofile(dataset)
 
+NUM_MAX_EXAMPLES_PER_DATA_BATCH = 3000
+
 if __name__ == '__main__':
   if len(sys.argv) < 2:
     print "Usage : "
-    print "python clixsense_dataset_gen.py clixsense_images_folder"
+    print "python clixsense_dataset_gen.py data_dir"
     sys.exit(1)
 
   # args 1 is the folder path where clixsense images located.
-  images_dir = sys.argv[1]
-  if not os.path.exists(images_dir):
-    print images_dir + ' not existed '
+  data_dir = sys.argv[1]
+  if not os.path.exists(data_dir):
+    print data_dir + ' not existed '
     sys.exit(2);
 
+  eval_data = 0
+  if len(sys.argv) > 2:
+    eval_data = sys.argv[2]
+
   # dataset name is clixsense_data_batch.bin in the same folder
-  dataset = images_dir + '/clixsense_data_batch.bin'
+  if not eval_data:
+    dataset = data_dir + '/data_batch_1.bin'
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
+                 for i in xrange(1, 6)]
+  else:
+    filenames = [os.path.join(data_dir, 'test_batch.bin')]
+    dataset = data_dir + '/test_batch.bin'
+
+  for f in filenames:
+    if os.path.exists(f):
+      os.remove(f)
+
   dataset_f = open(dataset, "w+")
   if not dataset_f:
     print "unable to open " + dataset + " with w+ mode"
     sys.exit(3)
-  for lists in os.listdir(images_dir):
-    path = os.path.join(images_dir, lists)
+
+  cc = 0
+  dc = 1
+  for lists in os.listdir(data_dir):
+    path = os.path.join(data_dir, lists)
     if lists.find("cat") >= 0:
       iscat = 1
     else:
       iscat = 0
     if os.path.splitext(lists)[1] == ".jpg":
+      if not eval_data:
+        if (dc < 5 and cc >= NUM_MAX_EXAMPLES_PER_DATA_BATCH):
+          cc = 0
+          dc = dc + 1
+          dataset_f.close()
+          dataset = data_dir + '/data_batch_' + str(dc) + '.bin'
+          dataset_f = open(dataset, "w+")
+        cc = cc + 1
       append_clixsense_dataset(path, dataset_f, iscat)
   dataset_f.close()
 
